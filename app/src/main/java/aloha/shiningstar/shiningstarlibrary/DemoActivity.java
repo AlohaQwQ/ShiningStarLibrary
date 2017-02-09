17a5e7b698ba607e917ecf83d4f0d5f520ac9623
AlohaQwQ <757993938@qq.com>
2017-02-09 10:16:40 +0800

    Local Commit
    Local Commit

---
+++ app/src/main/java/aloha/shiningstar/shiningstarlibrary/DemoActivity.java
@@ -0,0 +1,22 @@
+package aloha.shiningstar.shiningstarlibrary;
+
+import aloha.shiningstarbase.base.BaseActivity;
+import aloha.shiningstarbase.base.BasePresenter;
+
+public class DemoActivity extends BaseActivity {
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected BasePresenter CreatePresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/aloha/shiningstar/shiningstarlibrary/DemoFragment.java
@@ -0,0 +1,28 @@
+package aloha.shiningstar.shiningstarlibrary;
+
+import android.os.Bundle;
+
+import aloha.shiningstarbase.base.BaseFragment;
+import aloha.shiningstarbase.base.BasePresenter;
+
+/**
+ * Created by AlohaQoQ on 2017/2/7.
+ */
+
+public class DemoFragment extends BaseFragment {
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected BasePresenter CreatePresenter() {
+        return null;
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return 0;
+    }
+}
